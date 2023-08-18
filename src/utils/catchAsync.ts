e8d1aa845ccc4df8ee7378663eddf2ca4d01c717
George <george52016@hotmail.com>
2023-08-18 21:45:02 +0300

    fixing

---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'; 
+
+const wrapAsync = (func: (req: Request, res: Response, next: NextFunction) => Promise<any>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        func(req, res, next).catch(e => next(e));
+    };
+};
+
+export default wrapAsync;
