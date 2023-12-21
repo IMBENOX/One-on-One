baaa6007ad23c33c9c01443ee4dde069b0f064a2
George Apostolakis <grg.apost@gmail.com>
2023-12-21 17:30:32 +0200

    Seeding and Starting with one instruction

--- public/scripts/seeds/seed.js
+++ public/scripts/seeds/seed.js
@@ -1,8 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const categories_1 = require("./categories");
 const providers_1 = require("./providers");
-//Seeding Categories
-(0, categories_1.insertCategories)();
-//Seeding Providers
-(0, providers_1.insertProvidersServices)();
+const mongoose_1 = __importDefault(require("mongoose"));
+const seedDatabase = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield mongoose_1.default.connect('mongodb://127.0.0.1:27017/OneOnOneDb');
+        console.log("Database Connected");
+        // Seeding Categories
+        yield (0, categories_1.insertCategories)();
+        // Seeding Providers
+        yield (0, providers_1.insertProvidersServices)();
+        // Close the database connection
+        yield mongoose_1.default.disconnect();
+        console.log("Database Disconnected");
+    }
+    catch (err) {
+        console.error("Error seeding database:", err);
+    }
+});
+seedDatabase();
--- src/seeds/seed.ts
+++ src/seeds/seed.ts
@@ -1,13 +1,24 @@
 import {insertCategories} from "./categories";
 import {insertProvidersServices } from "./providers";
+import mongoose from "mongoose";
 
-
-
-//Seeding Categories
-insertCategories();
-
-//Seeding Providers
- insertProvidersServices();
-
-
-
+ const seedDatabase = async () => {
+    try {
+      await mongoose.connect('mongodb://127.0.0.1:27017/OneOnOneDb');
+      console.log("Database Connected");
+  
+      // Seeding Categories
+      await insertCategories();
+  
+      // Seeding Providers
+      await insertProvidersServices();
+  
+      // Close the database connection
+      await mongoose.disconnect();
+      console.log("Database Disconnected");
+    } catch (err) {
+      console.error("Error seeding database:", err);
+    }
+  };
+  
+  seedDatabase();
